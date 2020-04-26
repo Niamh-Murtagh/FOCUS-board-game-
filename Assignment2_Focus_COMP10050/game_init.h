14b53c9930dfdaf6b2ff96e6a612a6c40773741a
Niamh-Murtagh <62614055+Niamh-Murtagh@users.noreply.github.com>
2020-04-26 14:57:34 +0100

    Add files via upload
    This is the final version of my assignment changes were made to turns.c and checkwin.c

--- Assignment2_Focus_COMP10050/game_init.h
+++ Assignment2_Focus_COMP10050/game_init.h
@@ -57,5 +57,9 @@ void initialize_players(player players[PLAYERS_NUM]);
 //Function to create the board
 void initialize_board(square board[BOARD_SIZE][BOARD_SIZE]);
 
+void set_green(square * s);
+
+void set_red(square * s);
+
 
 #endif //ASSIGNMENT2_FOCUS_COMP10050_GAME_INIT_H
